e6c95aa1eab9274e72c750b08889c312d2e509d6
vinnadipo09 <vinnadipo09@gmail.com>
2018-11-02 14:29:08 +0300

    commit last2 friday

--- client/src/components/pages/Offers.vue
+++ client/src/components/pages/Offers.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
+    <div>
+      <HomeHeader/>
+    </div>
+    <div>
+      <v-parallax src="../static/page8.png">
+      </v-parallax>
+    </div>
+    <div>
+      <Footer class="mt-4"/>
+    </div>
   </div>
 </template>
 
 <script>
+import HomeHeader from '@/components/shared_components/HomeHeader.vue'
+import Panel from '@/components/shared_components/Panel.vue'
+import Footer from '@/components/shared_components/Footer.vue'
 export default {
   props: [
     'title'
-  ]
+  ],
+   components: {
+    HomeHeader,
+    Panel,
+    Footer
+  }
 }
 </script>
 
